51e4e403c8a6be83d252a7e127d7793820b0629f
Ezpi <104318978+ezp1ware@users.noreply.github.com>
2024-06-12 21:21:15 -0500

    Service comentarios valoraciones

--- src/app/services/comentariosvaloracionesrutas.service.ts
+++ src/app/services/comentariosvaloracionesrutas.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Subject } from 'rxjs';
+import { ComentariosValoracionesRutas } from '../models/comentariosvaloracionesrutas';
+import { HttpClient } from '@angular/common/http';
 
+const base_url = environment.base;
 @Injectable({
   providedIn: 'root'
 })
 export class ComentariosvaloracionesrutasService {
+  private url = `${base_url}/rutas_seguras`;
+  private listacambio = new Subject<ComentariosValoracionesRutas[]>();
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  list() {
+    return this.http.get<ComentariosValoracionesRutas[]>(this.url);
+  }
+  insert(cvr: ComentariosValoracionesRutas) {
+    return this.http.post(this.url, cvr);
+  }
+  setList(listaNueva: ComentariosValoracionesRutas[]) {
+    this.listacambio.next(listaNueva);
+  }
+  getList() {
+    return this.listacambio.asObservable();
+  }
+  listId(id:number){
+    return this.http.get<ComentariosValoracionesRutas>(`${this.url}/${id}`)
+  }
+  update(cvr: ComentariosValoracionesRutas){
+    return this.http.put(this.url, cvr);
+  }
+  eliminar(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
 }
