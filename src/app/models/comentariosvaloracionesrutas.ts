a0de36d5d543486233765f5d0249078e4040390c
Ezpi <104318978+ezp1ware@users.noreply.github.com>
2024-06-12 20:15:09 -0500

    Creacion model comentarios valoraciones rutas

---
+++ src/app/models/comentariosvaloracionesrutas.ts
@@ -0,0 +1,8 @@
+import { HistorialNavegacion } from "./historialnavegacion";
+
+export class ComentariosValoracionesRutas {
+    id: number = 0;
+    valoracion: number = 0;
+    comentario: string = '';
+    historialnavegacionid: HistorialNavegacion = new HistorialNavegacion();
+}
